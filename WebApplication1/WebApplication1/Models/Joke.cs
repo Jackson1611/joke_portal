235272faa7e68a267b075a8c68b093ae6330c711
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-02-14 16:39:58 +0200

    MVC project made with model created

---
+++ WebApplication1/WebApplication1/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int JokeQuestion { get; set; }
+        public int JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+
+    }
+}
