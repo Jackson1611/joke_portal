f577f9fc536877ec75b2ef5d670a943f1b1987fe
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-02-15 10:58:24 +0200

    added search feauture

---
+++ Joke Portal/Joke Portal/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Joke_Portal.Models;
+
+namespace Joke_Portal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Joke_Portal.Models.Joke> Joke { get; set; } = default!;
+    }
+}
